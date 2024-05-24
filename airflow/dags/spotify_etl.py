4dc7ba8b510f90cb646f2da9babc8cfe502636eb
StevenLomon <steven.lennartsson@gmail.com>
2024-05-24 10:44:54 +0000

    Getting started on spotify_etl.py

--- airflow/dags/spotify_etl.py
+++ airflow/dags/spotify_etl.py
@@ -30,5 +30,33 @@ def get_secret():
 client_secrets = get_secret()
 CLIENT_ID = client_secrets['CLIENT_ID']
 CLIENT_SECRET = client_secrets['CLIENT_SECRET']
-print(CLIENT_ID)
-print(CLIENT_SECRET)
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# get the 50 most popular featured playlists in Sweden right now
+r = requests.get(BASE_URL + 'browse/featured-playlists',
+                 headers=headers,
+                 params={'q': 'top%20%20tracks', 'type': 'playlist',
+                         'market': 'SE', 'limit': 50})
+d = r.json()
+
+# RAW DATA FOR THE 50 PLAYLISTS
+pd.DataFrame(d).to_csv("spotify_playlists_raw.csv", index=False)
