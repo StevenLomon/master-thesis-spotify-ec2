cc45b575d19e759b5f37ef59161415f92e65b30c
StevenLomon <steven.lennartsson@gmail.com>
2024-05-26 09:05:19 +0000

    Updated ETL functions

--- airflow/dags/spotify_dag.py
+++ airflow/dags/spotify_dag.py
@@ -12,46 +12,26 @@ target_bucket_name = 'spotify-airflow-etl-bucket'
 
 def extract_data():
     raw_playlist_data = extract_raw_playlist_data()
-    df = pd.DataFrame(raw_playlist_data['playlists']['items'])
+    df = pd.DataFrame(raw_playlist_data['tracks']['items'])
     now = datetime.now()
     date_now_string = now.strftime('%Y-%m-%d')
-    file_str = f"spotify_playlist_raw_data_{date_now_string}"
+    file_str = f"spotify_global50_raw_data_{date_now_string}"
     df.to_csv(f"{file_str}.csv", index=False)
     output_file_path = f"/home/ubuntu/{file_str}.csv"
-    return [output_file_path, file_str, raw_playlist_data]
+    return [output_file_path, file_str]
 
 def transform_data(task_instance):
-    data = task_instance.xcom_pull(task_ids='tsk_extract_spotify_data')[2]
+    data = task_instance.xcom_pull(task_ids='tsk_extract_spotify_data')[0]
     object_key_base = task_instance.xcom_pull(task_ids='tsk_extract_spotify_data')[1]
-    print("Data and object_key_base pulled from XCom:", data, object_key_base)
 
-    clean_playlist_data = transform_raw_playlist_data(data)
-    final_clean_data = transform_data_final(clean_playlist_data)
+    clean_global50_data = transform_raw_playlist_data(data)
 
     # Convert DataFrame to CSV format
-    csv_data = clean_playlist_data.to_csv(index=False)
-
-    # Convert DataFrames to Parquet format and store in memory buffer
-    parquet_buffer = BytesIO()
-    final_clean_data.to_parquet(parquet_buffer, index=False)
-    print("Parquet buffer created")
-
-    parquet_buffer.seek(0)  # Rewind the buffer
-    print("Parquet buffer seek done")
+    csv_data = clean_global50_data.to_csv(index=False)
 
     # Upload CSV to S3
     object_key = f"{object_key_base}.csv"
     s3_client.put_object(Bucket=target_bucket_name, Key=object_key, Body=csv_data)
-
-    # Upload paruqet to S3
-    object_key = f"spotify_data_final.parquet"
-    s3_client.put_object(Bucket=target_bucket_name, Key=object_key, Body=parquet_buffer.getvalue())
-    print("Parquet uploaded to S3")
-
-    # Return refined_tracks.json to be uploaded to S3 via BashOperator
-    file_str = "refined_tracks.json"
-    output_file_path = f"/home/ubuntu/{file_str}"
-    return [output_file_path, file_str]
 
 default_args = {
     'owner': 'airflow',
@@ -66,7 +46,7 @@ default_args = {
 
 with DAG('spotify_etl_dag',
          default_args=default_args,
-         # schedule_interval = @weekly,
+         schedule_interval = '@daily',
          catchup=False) as dag:
     
         extract_spotify_data = PythonOperator(
@@ -84,9 +64,4 @@ with DAG('spotify_etl_dag',
             bash_command= 'aws s3 mv {{ ti.xcom_pull("tsk_extract_spotify_data")[0] }} s3://spotify-airflow-etl-bucket-raw'
         )
 
-        load_json_intermediate_data_to_s3 = BashOperator(
-             task_id= 'tsk_load_json_intermediate_data_to_s3',
-             bash_command= f'aws s3 mv {{ ti.xcom_pull("tsk_transform_spotify_data")[0] }} s3://spotify-airflow-etl-bucket'
-        )
-
-        extract_spotify_data >> transform_spotify_data >> load_json_intermediate_data_to_s3 >> load_raw_data_to_s3
+        extract_spotify_data >> transform_spotify_data >> load_raw_data_to_s3
--- airflow/dags/spotify_etl.py
+++ airflow/dags/spotify_etl.py
@@ -19,7 +19,6 @@ def extract_raw_playlist_data():
                             'market': 'SE', 'limit': 1})
     d = q.json()
     global_top_50_id = d['playlists']['items'][0]['id']
-    global_top_50_id
 
     r = requests.get(BASE_URL + f'playlists/{global_top_50_id}',
                     headers=headers,
@@ -27,152 +26,18 @@ def extract_raw_playlist_data():
     d = r.json()
     return d
 
-def transform_raw_playlist_data(raw_playlist_data):
-    refined_playlists = []
-    for playlist in raw_playlist_data['playlists']['items']:
-        refined_playlists.append({'id': playlist['id'], 'name': playlist['name'],
-                                'description': playlist['description'],
-                                'owner': playlist['owner']['display_name'],
-                                'tracks': playlist['tracks']['total']})
-    df = pd.json_normalize(refined_playlists)
-
-    # Filter out rows where 'sleep' or 'noise' or 'peaceful' appears in the 'name' column (case-insensitive)
-    pattern = r'sleep|noise|peaceful'
-    df = df[~df['name'].str.contains(pattern, case=False)]
-
-    return df
-
-def extract_track_info(track_info, playlist_id, playlist_name, headers, session):
-    artist_id = track_info['artists'][0]['id']
-    album_id = track_info['album']['id']
-
-    endpoints = {
-        'track_info': f'tracks/{track_info["id"]}',
-        'artist_info': f'artists/{artist_id}',
-        'album_info': f'albums/{album_id}',
-        'audio_features': f'audio-features/{track_info["id"]}'
-    }
-
-    responses = {}
-    for key, endpoint in endpoints.items():
-        r = session.get(BASE_URL + endpoint, params={'market': 'SE'} if 'track_info' in key else {})
-        if r.status_code == 429:
-            print("Rate limit hit")
-            return None
-        if r.status_code == 200:
-            responses[key] = r.json()
-        else:
-            responses[key] = {}
-            print(f"Error: {r.status_code} - {r.text}")
-
-    return {
-        'id': track_info['id'],
-        'artist id': artist_id,
-        'album id': album_id,
-        'playlist id': playlist_id,
-        'name': track_info['name'],
-        'popularity': track_info['popularity'],
-        'genres': responses['artist_info'].get('genres', []),  # Extract genres from the artist info
-        'artist': track_info['artists'][0]['name'],
-        'artist popularity': responses['artist_info'].get('popularity'),
-        'album': responses['album_info'].get('name'),
-        'album release date': responses['album_info'].get('release_date'),
-        'album popularity': responses['album_info'].get('popularity'),
-        'playlist': playlist_name,
-        'danceability': responses['audio_features'].get('danceability'),
-        'energy level': responses['audio_features'].get('energy'),
-        'instrumentalness': responses['audio_features'].get('instrumentalness'),
-        'liveness': responses['audio_features'].get('liveness'),
-        'loudness': responses['audio_features'].get('loudness'),
-        'speechiness': responses['audio_features'].get('speechiness'),
-        'tempo': responses['audio_features'].get('tempo'),
-        'duration_ms': responses['audio_features'].get('duration_ms'),
-        'time signature': responses['audio_features'].get('time_signature')
-    }
-
-def get_refined_tracks(df, headers, num_rows=None, resume_from=None, save_interval=2, save_path=f'/home/ubuntu/refined_tracks.json'):
-    refined_tracks = []
-
-    try:
-        refined_tracks = pd.read_json(save_path).to_dict('records')
-        processed_playlists = set(track['playlist id'] for track in refined_tracks)
-        print("Resuming from saved progress.")
-    except (FileNotFoundError, ValueError) as e:
-        print("Save file not found or empty. Starting from scratch.")
-        processed_playlists = set()
-
-    resume_from = resume_from or 0
-
-    with requests.Session() as session:
-        session.headers.update(headers)
-
-        end_index = resume_from + (num_rows or len(df) - resume_from)
-        for index, row in df.iloc[resume_from:end_index].iterrows():
-            playlist_id = row['id']
-            playlist_name = row['name']
-            if playlist_id in processed_playlists:
-                print(f"Skipping already processed playlist: {playlist_id}")
-                continue
-
-            print(f"Processing playlist {index}: {playlist_id}")
-
-            r1 = session.get(BASE_URL + f'playlists/{playlist_id}/tracks', params={'market': 'SE'})
-            if r1.status_code == 429:
-                print("Rate limit hit")
-                pd.DataFrame(refined_tracks).to_json(save_path, orient='records')
-                return pd.json_normalize(refined_tracks)
-
-            if r1.status_code == 200:
-                d = r1.json()
-                for track in d.get('items', []):
-                    track_info = track.get('track')
-                    if track_info:
-                        refined_track = extract_track_info(track_info, playlist_id, playlist_name, headers, session)
-                        if refined_track:
-                            refined_tracks.append(refined_track)
-            else:
-                print(f"Error: {r1.status_code} - {r1.text}")
-
-            # Save intermediate results
-            if (index + 1) % save_interval == 0:
-                pd.DataFrame(refined_tracks).to_json(save_path, orient='records')
-                print(f"Saved progress after processing {index + 1} playlists")
-
-    # Final save
-    pd.DataFrame(refined_tracks).to_json(save_path, orient='records')
-    return pd.json_normalize(refined_tracks)
-
-def transform_data_final(clean_playlist_data):
-    df = get_refined_tracks(clean_playlist_data, headers, num_rows=None)
-
-    # Group by the unique track ID
-    df_aggregated = df.groupby('id').agg({
-        'artist id': 'first', # We can safely use 'first' since 'id' guarantees uniqueness
-        'album id': 'first',
-        'name': 'first',
-        'popularity': 'first',
-        'genres': 'first',
-        'artist': 'first',
-        'artist popularity': 'first',
-        'album': 'first',
-        'album release date': 'first',
-        'album popularity': 'first',
-        'playlist': lambda x:list(set(x)),  # Compile all playlist names into a list
-        'danceability': 'mean',  # Average track features if there's variation
-        'energy level': 'mean',
-        'instrumentalness': 'mean',
-        'liveness': 'mean',
-        'loudness': 'mean',
-        'speechiness': 'mean',
-        'tempo': 'mean',
-        'duration_ms': 'mean',
-        'time signature': 'mean'
-    }).reset_index()
-
-    # Rename the aggregated playlist column for clarity
-    df_aggregated.rename(columns={'playlist': 'playlist sources'}, inplace=True)
-
-    # Column with occurance count
-    df_aggregated['playlist occurrences'] = df_aggregated['playlist sources'].apply(len)
-
-    return df_aggregated
+def transform_raw_playlist_data(raw_playlist_df):
+    df = raw_playlist_df
+    df['id'] = df['track'].apply(lambda x: x['id'])
+    df['name'] = df['track'].apply(lambda x: x['name'])
+    df['popularity'] = df['track'].apply(lambda x: x['popularity'])
+    df['explicit'] = df['track'].apply(lambda x: x['explicit'])
+    df['duration_ms'] = df['track'].apply(lambda x: x['duration_ms'])
+    df['external_url'] = df['track'].apply(lambda x: x['external_urls']['spotify'])
+    df['artist'] = df['track'].apply(lambda x: x['artists'][0]['name'])
+    df['artist_id'] = df['track'].apply(lambda x: x['artists'][0]['id'])
+    df['album'] = df['track'].apply(lambda x: x['album']['name'])
+    df['album_id'] = df['track'].apply(lambda x: x['album']['id'])
+    df['album_release_date'] = df['track'].apply(lambda x: x['album']['release_date'])
+    df_cleaned = df.drop(columns=['added_at', 'is_local', 'primary_color', 'added_by', 'track', 'video_thumbnail'])
+    return df_cleaned
