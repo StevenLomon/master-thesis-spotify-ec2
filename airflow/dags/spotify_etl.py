b2f9b7d9fbbf364e9bb910dc4f03e303ce63cb3d
StevenLomon <steven.lennartsson@gmail.com>
2024-05-30 06:25:45 +0000

    I honestly don't know what changed since last time :')

--- airflow/dags/spotify_etl.py
+++ airflow/dags/spotify_etl.py
@@ -26,31 +26,6 @@ def extract_raw_playlist_data():
     return d
 
 def transform_raw_playlist_data(raw_playlist_data):
-    # The following didn't work for some reason????
-    # # Helper function to safely extract values from the track dictionary
-    # def get_track_value(track, key):
-    #     if isinstance(track, dict):
-    #         return track.get(key, None)
-    #     return None
-    
-    # # Extract values with safety checks
-    # raw_playlist_df['id'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x, 'id'))
-    # raw_playlist_df['name'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x, 'name'))
-    # raw_playlist_df['popularity'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x, 'popularity'))
-    # raw_playlist_df['explicit'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x, 'explicit'))
-    # raw_playlist_df['duration_ms'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x, 'duration_ms'))
-    # raw_playlist_df['external_url'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x.get('external_urls', {}), 'spotify') if isinstance(x, dict) else None)
-    # raw_playlist_df['artist'] = raw_playlist_df['track'].apply(lambda x: x['artists'][0]['name'] if isinstance(x, dict) and 'artists' in x and len(x['artists']) > 0 else None)
-    # raw_playlist_df['artist_id'] = raw_playlist_df['track'].apply(lambda x: x['artists'][0]['id'] if isinstance(x, dict) and 'artists' in x and len(x['artists']) > 0 else None)
-    # raw_playlist_df['album'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x.get('album', {}), 'name') if isinstance(x, dict) else None)
-    # raw_playlist_df['album_id'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x.get('album', {}), 'id') if isinstance(x, dict) else None)
-    # raw_playlist_df['album_release_date'] = raw_playlist_df['track'].apply(lambda x: get_track_value(x.get('album', {}), 'release_date') if isinstance(x, dict) else None)
-
-    # # Drop the original nested columns
-    # df_cleaned = raw_playlist_df.drop(columns=['added_at', 'is_local', 'primary_color', 'added_by', 'track', 'video_thumbnail'])
-
-    # return df_cleaned
-
     refined_playlist_tracks = []
     for item in raw_playlist_data['tracks']['items']:
         track_data = item['track']
